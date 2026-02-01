d8fd8c97916c8359ac361c89ffc2a38bc5d1da87
Raslan Rizvi <raslanrizvi05@gmail.com>
2026-02-01 22:57:34 +0530

    Imported Navbar Component

--- client/src/app/dashboardWrapper.tsx
+++ client/src/app/dashboardWrapper.tsx
@@ -1,10 +1,16 @@
 import React from "react";
+import Navbar from "@/app/(components)/Navbar";
 
 const dashboardWrapper = ({ children }: { children: React.ReactNode }) => {
   return (
     <div className="flex min-h-screen w-full bg-gray-50 text-gray-900">
       {/* SideBar */}
-      <main className="flex w-full flex-col bg-gray-50 dark:bg-dark-bg md:pl-64"></main>
+      <main
+        className={`flex w-full flex-col bg-gray-50 dark:bg-dark-bg md:pl-64`}
+      >
+        <Navbar />
+        {children}
+      </main>
     </div>
   );
 };
