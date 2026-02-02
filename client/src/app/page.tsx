fdd326bde3d5f18544f8c136b0c413f37be20c04
Raslan Rizvi <raslanrizvi05@gmail.com>
2026-02-02 17:20:32 +0530

    Minor chamges to style

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,9 +1,9 @@
-import Image from "next/image"
+import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className='flex min-h-screen flex-col items-center justify-between p-24 bg-dark-bg text-white'>
+    <main className="flex min-h-screen flex-col items-center justify-between p-24 text-gray-900 dark:bg-dark-bg dark:text-white">
       Hi
     </main>
-  )
+  );
 }
